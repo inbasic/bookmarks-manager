efb5b8fc1207fea961378c59f41c3dfbb944fe01
InBasic <inb.cor@gmail.com>
2017-09-05 10:52:30 +0430

    updating LICENSE headers based on https://github.com/inbasic/bookmarks-manager/issues/6#issuecomment-326998184

--- data/options/index.js
+++ data/options/index.js
@@ -1,19 +1,29 @@
+/* Copyright (C) 2014-2017 InBasic
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Home: http://add0n.com/bookmarks-manager.html
+ * GitHub: https://github.com/inbasic/bookmarks-manager/
+*/
+
 'use strict';
 
 var log = document.getElementById('status');
 
-function restore_options () {
+function restore() {
   chrome.storage.local.get({
     width: 400,
     height: 600
-  }, (prefs) => {
-    Object.keys(prefs).forEach (name => {
+  }, prefs => {
+    Object.keys(prefs).forEach(name => {
       document.getElementById(name)[typeof prefs[name] === 'boolean' ? 'checked' : 'value'] = prefs[name];
     });
   });
 }
 
-function save_options() {
+function save() {
   let prefs = {
     width: Math.max(300, document.getElementById('width').value),
     height: Math.max(400, document.getElementById('height').value)
@@ -22,14 +32,14 @@ function save_options() {
   chrome.storage.local.set(prefs, () => {
     log.textContent = 'Options saved.';
     setTimeout(() => log.textContent = '', 750);
-    restore_options();
+    restore();
   });
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
+document.addEventListener('DOMContentLoaded', restore);
 document.getElementById('save').addEventListener('click', () => {
   try {
-    save_options();
+    save();
   }
   catch (e) {
     log.textContent = e.message;
